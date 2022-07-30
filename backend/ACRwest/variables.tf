abe703c8f984546304c930ce4af1a80395caf5c0
danlhennessy <danh118@hotmail.co.uk>
2022-07-31 00:57:46 +0100

    alt deploy location - ukwest

---
+++ backend/ACRwest/variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+  type        = string
+  default     = "devopsthehardway"
+  description = "Name for resources"
+}
+
+variable "location" {
+  type        = string
+  default     = "west"
+  description = "Azure Location of resources"
+}
---
+++ backend/LogAnalyticswest/variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+  type        = string
+  default     = "devopsthehardway"
+  description = "Name for resources"
+}
+
+variable "location" {
+  type        = string
+  default     = "ukwest"
+  description = "Azure Location of resources"
+}
